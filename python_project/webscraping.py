b32da20b9d61dc5eb7459d162a37ec729255159d
Filipe Vaz <filipecraveiravaz@gmail.com>
2024-06-26 17:09:08 +0100

    mysql connection

--- python_project/webscraping.py
+++ python_project/webscraping.py
@@ -1,10 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import mysql.connector
 
 
+userconn = mysql.connector.connect(host= "localhost", user= "filipe", password= "12345")
 
+def checkDB():
+    cursor = userconn.cursor(buffered=True)
+    cursor.execute("SHOW DATABASES")
+    for x in cursor:
+        if "bd_mercados" not in x:
+            cursor.execute("CREATE DATABASE bd_mercados")
+        else:
+            break
+    
+dbconn = mysql.connector.connect(host= "localhost", user= "filipe", password= "12345", database="bd_mercados")
+def createTables():
+    cursor = dbconn.cursor(buffered=True)
+    cursor.execute("""CREATE TABLE produtos(
+                           id int AUTO_INCREMENT PRIMARY KEY, 
+                           nome_super varchar(255), 
+                           nome_prod varchar(255), 
+                           categoria_prod varchar(255), 
+                           preco int, 
+                           marca_prod varchar(255))""")
+        
 def continente():
+    cursor = dbconn.cursor()
     urls=['https://www.continente.pt/produto/cebola-roxa-continente-5394157.html',
           'https://www.continente.pt/produto/batata-vermelha-continente-5454736.html',
           'https://www.continente.pt/produto/banana-continente-2597619.html',
@@ -30,25 +52,29 @@ def continente():
     for url in urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-
+        loja = "Continente"
         nome = soup.find('h1',class_='pwc-h3 col-h3 product-name pwc-font--primary-extrabold mb-0').get_text(strip=True)
-        print('nome:',nome)
+
         categoria = soup.find('span',class_='breadcrumb-fill',itemprop='name').get_text(strip=True)
-        print('categoria',categoria)
+
         preco = soup.find('span',class_='ct-price-formatted').get_text(strip=True)
-        print('preço:',preco)
-        loja = soup.find('a',class_='ct-pdp--brand col-pdp--brand').get_text(strip=True)
-        print('loja:',loja,'\n ------------------------')
-        
+
+        marca = soup.find('a',class_='ct-pdp--brand col-pdp--brand').get_text(strip=True)
+    
+        sql = "INSERT into produtos (nome_super, nome_prod, categoria_prod, preco, marca_prod) VALUES(%s, %s, %s, %s, %s)"
+        val = (loja, nome, categoria, preco, marca)
+        cursor.execute(sql, val)
+
+    dbconn.commit()
 
 def auchan():
+    cursor = dbconn.cursor()
     urls = ['https://www.auchan.pt/pt/produtos-frescos/legumes/batatas-alho-e-cebola/batata-vermelha-auchan-3-kg/3483188.html',
             'https://www.auchan.pt/pt/produtos-frescos/legumes/batatas-alho-e-cebola/cebola-kg/21946.html',
             'https://www.auchan.pt/pt/produtos-frescos/fruta/banana-e-frutos-tropicais/banana-importada-kg/234229.html',
             'https://www.auchan.pt/pt/alimentacao/produtos-lacteos/leites/leite-uht/leite-mimosa-uht-meio-gordo-1l/11885.html',
             'https://www.auchan.pt/pt/produtos-frescos/fruta/macas-peras-e-uvas/maca-gala-auchan-kg/3356631.html',
             'https://www.auchan.pt/pt/produtos-frescos/fruta/macas-peras-e-uvas/pera-rocha-auchan-kg/3356405.html',
-            # 'https://www.auchan.pt/pt/produtos-frescos/fruta/laranjas-clementinas-e-limoes/laranja-do-algarve-igp-auchan-kg/3374907.html'
             'https://www.auchan.pt/pt/produtos-frescos/fruta/laranjas-clementinas-e-limoes/laranja-do-algarve-igp-auchan-cultivamos-o-bom-1.5-kg/3274212.html'
             'https://www.auchan.pt/pt/produtos-frescos/queijaria/queijo-fatiado-e-barra/queijo-flamengo-auchan-a-mesa-em-portugal-acores-fatias-500g/3352009.html',
             'https://www.auchan.pt/pt/produtos-frescos/legumes/abobora-cenoura-e-alho-frances/cenoura-auchan-kg/3374234.html',
@@ -63,28 +89,30 @@ def auchan():
             'https://www.auchan.pt/pt/produtos-frescos/ovos/ovos-de-galinhas-criadas-no-solo/ovos-auchan-galinhas-solo-classe-m-uma-duzia/2945539.html',
             'https://www.auchan.pt/pt/beleza-e-higiene/papel-higienico-e-lencos-papel/papel-higienico-eco-e-premium/papel-higienico-auchan-ecologico-2-folhas-100-pasta-reciclada-12-rolos-%3D-24-rolos/3347366.html',
             'https://www.auchan.pt/pt/alimentacao/mercearia/batatas-fritas-e-aperitivos-snacks/batatas-com-sabores/batatas-auchan-fritas-girassol-camponesas-150g/1182735.html']
-    print("--------------------------------------------------------------")
-    loja = "Auchan"
-    print(loja)
     for url in urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        print()
-        print("--------------------------------------------------------------")
-        print()
+
+        loja = "Auchan"
+        
         nome = soup.find('h1', class_="product-name auc-hero-title").get_text(strip=True)
-        print("Nome: ", nome)
-        print()        
+
         spanParent = soup.find('span', class_="sales")
         preco = spanParent.findChild('span', class_="value").get_text(strip=True)
-        print("Preço: ", preco)
-        print()
-        categorias = soup.find_all(class_="breadcrumb-item")
-        subcat = categorias[1].text 
-        print(subcat)
-    print("--------------------------------------------------------------")
+
+        categoria = soup.find_all(class_="breadcrumb-item")
+        subcat = categoria[1].text 
+
+        marca = "Auchan"
+
+        sql = "INSERT into produtos (nome_super, nome_prod, categoria_prod, preco, marca_prod) VALUES(%s, %s, %s, %s, %s)"
+        val = (loja, nome, subcat, preco, marca)
+        cursor.execute(sql, val)
+
+    dbconn.commit()        
 
 def aldi():
+    cursor = dbconn.cursor()
     urls = ['https://www.aldi.pt/oportunidades-da-semana/a-partir-de-4-f-26-6/cebola-7005581-2-0.article.html',
             'https://www.aldi.pt/oportunidades-da-semana/a-partir-de-4-f-26-6/batata-roxa-nacional-7005571-2-0.article.html',
             'https://www.aldi.pt/produto/snack-crocante-de-cenoura-banana-biologico-7006070-1-0.article.html#/produtos/bem-estar-beleza-e-bebe/bebe',
@@ -109,18 +137,26 @@ def aldi():
     for url in urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
+        
+        loja = "Aldi"
 
         nome = soup.find('h1').get_text(strip=True)
-        print('nome:',nome)
+
         categoria = soup.find_all('li', class_='mod-breadcrumb__item')
         subcat = categoria[2].text
-        print('categoria:', subcat)
+
         preco = soup.find('span',class_='price__wrapper').get_text(strip=True)
-        print('preço:',preco)
-        loja = soup.find('span',class_='mod-article-intro__header-headline-small').get_text(strip=True)
-        print('loja:',loja,'\n ------------------------')
+
+        marca = soup.find('span',class_='mod-article-intro__header-headline-small').get_text(strip=True)
+        sql = "INSERT into produtos (nome_super, nome_prod, categoria_prod, preco, marca_prod) VALUES(%s, %s, %s, %s, %s)"
+        val = (loja, nome, subcat, preco, marca)
+        cursor.execute(sql, val)
+
+    dbconn.commit() 
 
 if __name__=='__main__':
+    checkDB()
+    createTables()
     continente()
     auchan()
     aldi()
